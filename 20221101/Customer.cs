cc6163806c4523687e5e696551b9995be4fc4fd1
malin <malin@DESKTOP-D4HUDUG>
2022-11-01 10:08:16 +0100

    Add project files.

---
+++ 20221101/Customer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20221101
+{
+    class Customer
+    {
+        private int _Id;
+        private string _FirstName;
+        private string _LastName;
+        private string _Email;
+
+
+
+        public int Id 
+        {
+            get { return _Id; }
+            set 
+            {
+                if (value <0)
+                {
+                    _Id = 0;
+                }
+                else
+                {
+                    _Id = value;
+                }
+            }
+        }
+        public string FirstName 
+        { 
+            get { return _FirstName; }
+            set 
+            { 
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("No Firstname");
+                }
+                else
+                {
+                    _FirstName = value;
+                }
+        }   }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("No Firstname");
+                }
+                else
+                {
+                    _LastName = value;
+                }
+            }
+        }
+        public string Email
+        {
+            get { return _Email; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Email is null or empty");
+                }
+                else if(value == null)
+                {
+                    throw new Exception("Email is empty");
+                }
+                else
+                {
+                    _Email = value;
+                }
+                
+            }
+        }
+        
+
+
+
+    }
+}
---
+++ 20221101/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _20221101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer();
+            Customer C2 = new Customer();
+            Customer C3 = new Customer();
+            
+
+            C1.Id = 100;
+            C1.FirstName = "Janne";
+            C1.LastName = "Johansson";
+            C1.Email = "Janne@gmail.com";
+
+            C2.Id = 200;
+            C2.FirstName = "Johan";
+            C2.LastName = "Eriksson";
+            C2.Email = "Johan@gmail.com";
+
+            C3.Id = -10;
+            C3.FirstName = "";
+            C3.LastName = "";
+            C3.Email = "";
+
+            Console.WriteLine($"Info Custumer one:{C1.Id}, {C1.FirstName}, {C1.LastName}, {C1.Email}");
+            Console.WriteLine($"Info Custumer two:{C2.Id}, {C2.FirstName}, {C2.LastName}, {C2.Email}");
+            Console.WriteLine($"Info Custumer three:{C3.Id}, {C3.FirstName}, {C3.LastName}, {C3.Email}");
+
+        }
+    }
+}
